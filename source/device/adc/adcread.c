675960ba965017a2737354678e805e15837d26f0
Rakesh Ranjan Jha <rakeshjha@ufl.edu>
2016-11-21 00:24:00 -0500

    COP5615-XINU Project: Added ADC driver support
    This patch adds ADC driver support AM335X.
    Test application connects temperature sensor to AIN3
    and uses ADC to read temperature. Test code is present
    in main. main and initialize.c have test changes to allow
    XINU bootup without ethernet and measure temperature.
    Revert only main and initialize if needed.
    
    Signed-off-by: Rakesh Ranjan Jha <rakeshjha@ufl.edu>

---
+++ source/device/adc/adcread.c
@@ -0,0 +1,49 @@
+/* adcread.c - adcread */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ *  adcread  -  Read one converted word from ADC
+ *------------------------------------------------------------------------
+ */
+devcall	adcread(
+	  struct dentry	*devptr,	/* Entry in device switch table	*/
+	  int32	*adcval,			/* Converted word - out param	*/
+	  int32	pin 			    /* ADC pin to read - AIN1 only  */
+	)
+{
+	struct	adc_csreg *adcreg = (struct	adc_csreg *)devptr->dvcsr;
+    int32 retries = 6;
+    uint32 sample = 0;
+    uint32 stepconfval = (ADC_STEP_CONFIG_VAL | (pin << ADC_CHANNEL_START_POS));
+
+    if((pin < 0) || (pin > 7))
+        return SYSERR;
+
+    kprintf("stepconfig = 0x%X\n", stepconfval);
+
+    /* Enable step 1 */
+    adcreg->stepenable = ADC_STEP1_ENABLE;
+
+    /* VREFN, AINX, VREFP, Data in FIFO 16 samples average, SW enabled, one shot */
+    adcreg->stepconfig1 = stepconfval;
+
+    /* TSC_ADC_SS module enabled */
+    adcreg->ctrl = ADC_MODULE_EN;
+
+    /* Waiting for MODULEMODE field to reflect the written value. */
+    while(ADC_MODULE_DIS == (adcreg->ctrl & ADC_MODULE_EN_MASK));
+
+    /* Now loop to check result ready */
+    while((retries-- > 0) &&
+        (adcreg->fifo0count & ADC_FIFO0COUNT_MASK) == 0)
+            sleepms(1);
+    if(retries > 0) {
+        /* Read data from fifo 0 */
+        sample = (adcreg->fifo0data & ADC_FIFODATA_MASK);
+        *adcval = (sample * ADC_VREF)/ADC_STEPS;
+    }
+    else
+        return SYSERR;
+
+}
--- source/system/main.c
+++ source/system/main.c
@@ -4,20 +4,18 @@
 
 process	main(void)
 {
-
-	/* Run the Xinu shell */
+    int32 mvSample, temp_c, res=SYSERR, times=5;
 
 	recvclr();
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-
-	/* Wait for shell to exit and recreate it */
-
-	while (TRUE) {
-		receive();
-		sleepms(200);
-		kprintf("\n\nMain process recreating shell\n\n");
-		resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
+	res = init(ADC);
+	while(times > 0) {
+        read(ADC, &mvSample, 3);
+        temp_c = (mvSample - 500)/10;
+        printf("Iteration-%d\n", (6 - times));
+        printf("The result is %d and sample in mv = %d and temp in deg cel = %d\n\n\n", res, mvSample, temp_c);
+        times-=1;
+        sleep(5);
 	}
+
 	return OK;
-    
 }
