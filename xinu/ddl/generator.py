dd94b184a4951f4efca67b6450884efdab4c3c57
Ankit Singh <akki.dos19@gmail.com>
2016-11-27 16:17:15 -0500

    Working DDL - integrated with XINU makefile
    Signed-off-by: Ankit Singh <akki.dos19@gmail.com>

--- xinu/ddl/generator.py
+++ xinu/ddl/generator.py
@@ -1,7 +1,7 @@
 def generate(tree):
     
     c_header = "#include<xinu.h>\n"
-    c_end = "return res;\n}"
+    c_end = "return 0;\n}"
     
 
 
@@ -26,10 +26,10 @@ def generate(tree):
                 chars = parameter.findall('Parameter_characterization')
                 lb = chars[0].text
                 ub = chars[1].text
-                c_cpp = c_header+"int32 do_"+method.text+"(){\nint32 p="+a2+", res;\nif((p<"+lb+")||(p>"+ub+")){\n"+c_stmt+"}\nres = " +method.text + "("+b+");\n" + c_end
+                c_cpp = c_header+"int32 do_"+method.text+"(){\nint32 p="+a2+";\nif((p<"+lb+")||(p>"+ub+")){\n"+c_stmt+"}\n" +method.text + "("+b+");\n" + c_end
                 c_prototype = "int32 do_" + method.text + "();"
             else:
-                c_cpp = c_header+"int32 do_"+method.text+"(){\nint32 res;\nres = "+method.text+"();\n"+c_end
+                c_cpp = c_header+"int32 do_"+method.text+"(){\nint32 res;\nres = "+method.text+"();\nreturn res;\n}"
                 c_prototype = "int32 do_"+method.text+"();"
         else:
             parameter = signal.find('Parameter')
@@ -37,33 +37,33 @@ def generate(tree):
             pvalue = parameter.find('Parameter_value').text
 
             if pvalue == 'ACCEL':
-                c_cpp = c_header+"int32 do_accel_"+method.text+"(){\nint32 res;\nres = "+method.text+"("+pvalue+");\n"+c_end
+                c_cpp = c_header+"int32 do_accel_"+method.text+"(){\n"+method.text+"("+pvalue+");\n"+c_end
                 c_prototype = "int32 do_accel_"+method.text+"();"
 
             elif pvalue == 'accel_d':
-                c_cpp = c_header+"int32 do_accel_"+method.text+"("+ptype+" *"+pvalue+"){\nint32 res;\nres = "+method.text+"("+pvalue+");\n"+"return res;\n}"
+                c_cpp = c_header+"int32 do_accel_"+method.text+"("+ptype+" *"+pvalue+"){\nint32 res;\nres = "+method.text+"(ACCEL, "+pvalue+", 0);\n"+"return res;\n}"
                 c_prototype = "int32 do_accel_"+method.text+"("+ptype+" *"+pvalue+");"
 
             else:
                 c_cpp = c_header+"int32 do_"+method.text+"("+ptype+" *"+pvalue+"){\nint32 res;\nres = "+method.text+"("+pvalue+");\n"+"return res;\n}"
                 c_prototype = "int32 do_"+method.text+"("+ptype+" *"+pvalue+");"
 
-        fileC = '/Users/ankitsingh/project/dosp/xinu/ddl/test_c/do_'+method.text+'.c'
+        fileC = '../system/do_'+method.text+'.c'
         fC = open(fileC, 'w+')
         fC.write(c_cpp)
         fC.close()
 
         fileH = 'do_'+method.text+'.h'
-        fdir = '/Users/ankitsingh/project/dosp/xinu/ddl/test_h/'+fileH+'.h'
-        fH = open(fileH, 'w+')
+        fdir = '../include/'+fileH
+        fH = open(fdir, 'w+')
         fH.write(c_prototype)
         fH.close()
 
         fhead = '#include <'+fileH+'>'
-        fx = open('/Users/ankitsingh/project/dosp/xinu/include/xinu.h', 'r')
+        fx = open('../include/xinu.h', 'r')
         f1 =fx.read()
         if fhead not in f1:
-            fy = open('/Users/ankitsingh/project/dosp/xinu/include/xinu.h', 'a+')
+            fy = open('../include/xinu.h', 'a+')
             fy.write('\n'+fhead)
             fy.close()
         fx.close()
